af1a31cea43730c54faee76f341764a6d1c28909
ko_no <90126004+MrKono@users.noreply.github.com>
2025-06-02 22:39:07 +0900

    spotless

--- src/main/java/kono/ceu/gtconsolidate/common/blocks/BlockCoACasing.java
+++ src/main/java/kono/ceu/gtconsolidate/common/blocks/BlockCoACasing.java
@@ -1,7 +1,5 @@
 package kono.ceu.gtconsolidate.common.blocks;
 
-import gregtech.api.block.VariantBlock;
-import gregtech.api.items.toolitem.ToolClasses;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
@@ -9,10 +7,14 @@ import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+
 import org.jetbrains.annotations.NotNull;
 
-public class BlockCoACasing  extends
-        VariantBlock<BlockCoACasing.CoACasingType> {
+import gregtech.api.block.VariantBlock;
+import gregtech.api.items.toolitem.ToolClasses;
+
+public class BlockCoACasing extends
+                            VariantBlock<BlockCoACasing.CoACasingType> {
 
     public BlockCoACasing() {
         super(Material.IRON);
